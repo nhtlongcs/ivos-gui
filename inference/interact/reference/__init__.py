ef4f7f4fd31d528f2bc42edf2b356e2ec93af7f7
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-08-02 06:06:33 +0000

    add cps

--- inference/interact/reference/__init__.py
+++ inference/interact/reference/__init__.py
@@ -1,2 +1,4 @@
 from inference.interact.reference.transunet import TransUnet
 from inference.interact.reference.transunet.transunet_pos import TransUnetPE
+from inference.interact.reference.cps import CrossPseudoSupervision
+from inference.interact.reference.segmodels import BaseSegModel
